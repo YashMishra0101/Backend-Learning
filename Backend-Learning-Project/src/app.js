9c3f322f7f10d77157b1463124dadef85c61a6b3
YashMishra0101 <yashrkm0011@gmail.com>
2025-11-10 20:00:49 +0530

    Back to study — started learning backend again (couldn’t study due to typhoid fever)

--- Backend-Learning-Project/src/app.js
+++ Backend-Learning-Project/src/app.js
@@ -11,8 +11,32 @@ app.use(
   }),
 );
 
+//Middleware 
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
 export { app };
+
+/*
+## Middlewares
+
+### 🧠 `app.use(express.json({ limit: "16kb" }))`
+
+This middleware allows your backend to accept JSON data from incoming requests — like POST, PUT, etc.
+
+### 🧠 `app.use(express.urlencoded({ extended: true, limit: "16kb" }))`
+
+It means — the maximum size of JSON payload allowed is 16 kilobytes.
+If someone tries to send too large a JSON (like 5MB), the server rejects it to protect from DoS attacks (hackers sending huge data to crash your backend).
+
+### 🧠 `app.use(express.static("public"))`
+
+This allows your server to handle form submissions (like data from HTML forms or URL-encoded requests).
+
+### 🧠 `app.use(cookieParser())`
+
+This allows your backend to serve static files (like images, CSS, JS, PDFs, etc.) directly from the "public" folder.
+
+*/
