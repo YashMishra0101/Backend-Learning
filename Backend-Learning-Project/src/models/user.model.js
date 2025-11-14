f79b8bd9acec478e7713d10634503a7ce0615b07
YashMishra0101 <yashrkm0011@gmail.com>
2025-11-14 20:45:55 +0530

    Learning Backend

---
+++ Backend-Learning-Project/src/models/user.model.js
@@ -0,0 +1,53 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //we are using cloudinary URL
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary URL
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = mongoose.model("User", userSchema);
