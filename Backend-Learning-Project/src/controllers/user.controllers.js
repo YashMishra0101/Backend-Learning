ee61b0194cf4a43a7a784d0aa6312c67dee19348
YashMishra0101 <yashrkm0011@gmail.com>
2025-12-03 21:54:37 +0530

    Mongodb aggregation pipelines

--- Backend-Learning-Project/src/controllers/user.controllers.js
+++ Backend-Learning-Project/src/controllers/user.controllers.js
@@ -337,25 +337,34 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
         isSubscribed: {
           $cond: {
             if: { $in: [req.user?._id, "$subscribers.subscriber"] },
-            then:true,
-            else:false
+            then: true,
+            else: false,
           },
         },
       },
     },
     {
-      $project:{
-        fullName:1,
-        userName:1,
-         subscribersCount:1,
-         channelsSubscribedToCount:1,
-          isSubscribed:1,
-          avatar:1,
-          coverImage:1,
-          email:1
-      }
-    }
+      $project: {
+        fullName: 1,
+        userName: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
   ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "channel does not exists");
+  }
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User channel fetched successfully"),
+    );
 });
 
 export {
